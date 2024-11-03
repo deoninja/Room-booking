a326ba03003d27c4b29bf762966924c26697233a
deo <deoninja@gmail.com>
2024-11-03 21:54:45 +0800

    Protecting Routes

--- middleware.js
+++ middleware.js
@@ -1,14 +1,19 @@
 import { NextResponse } from "next/server"; 
 
 export async function middleware(request) {
-    const {pathname} = request.nextUrl;
+    const isAuthenticated = true;
 
-    console.log(`Request Page: ${pathname}`);
+    if(!isAuthenticated){
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+    // const {pathname} = request.nextUrl;
+
+    // console.log(`Request Page: ${pathname}`);
 
     return NextResponse.next();
 }
 
 // run only on login
 export const config = {
-    matcher: ['/login'],
+    matcher: ['/bookings'],
 }
