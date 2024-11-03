93e755d052391f4b8b8217b7204c24033e0c13ab
deo <deoninja@gmail.com>
2024-11-03 22:15:49 +0800

    Login & Create Session

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,10 +1,20 @@
+'use client'
 import Link from "next/link";
+import { useEffect, useActionState } from "react";
+// import { useFormState}  from 'react-dom';
+import createSession from "../actions/createSession";
 
 const LoginPage = () => {
+  const [state, formAction] = useActionState(createSession, {});
+
+  useEffect(() => {
+    if(state.error) console.log(state.error)
+  }, [state])
+
   return (
     <div className="flex items-center justify-center">
       <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-sm mt-20">
-        <form>
+        <form action={formAction}>
           <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
             Login
           </h2>
@@ -18,7 +28,7 @@ const LoginPage = () => {
               id="email"
               name="email"
               className="border rounded w-full py-2 px-3"
-              required
+              
             />
           </div>
 
