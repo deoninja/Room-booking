67dbf3e2e6241f5f85df342cf30e3941f671ecc5
deo <deoninja@gmail.com>
2024-11-23 01:51:02 +0800

    BookedRoomCard Component

--- app/bookings/page.jsx
+++ app/bookings/page.jsx
@@ -1,14 +1,15 @@
 import Heading from "@/components/Heading";
 import getMyBookings from "../actions/getMyBookings";
+import BookedRoomCard from "@/components/BookedRoomCard";
 const BookingsPage = async () => {
     const bookings = await getMyBookings();
-    console.log('booking',bookings)
 
     return <>
         {bookings.length === 0 ? (
             <p className="text-gray-600 mt-4">You have no bookings</p>
         ) : (
-            bookings.map((booking) => <h3 key={booking.id}>{booking?.room_id?.name}</h3>)
+            bookings.map((booking) => <BookedRoomCard key={booking.$id}
+            booking={booking} />)
         )}
     </>;
 }
